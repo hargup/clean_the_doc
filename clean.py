f4f73e45c129d23026e7237bd087e0ec73563253
Harsh Gupta <gupta.harsh96@gmail.com>
2015-08-30 00:22:52 +0530

    Get Unstained image
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- clean.py
+++ clean.py
@@ -12,7 +12,8 @@ from skimage.morphology import square
 from skimage.filters import threshold_otsu, rank
 from skimage.util import img_as_ubyte
 from skimage.io import imread
-from skimage.measure import label
+from skimage.measure import label, regionprops
+from skimage.morphology import binary_erosion
 
 import os
 import numpy as np
@@ -45,31 +46,120 @@ def get_data(image_dir):
 
 def remove_small_components(img):
     # This method works well, just that the dot on i is going away
-    clusters = label(img, connectivity=2)
+    # Dialate the image first so that letters connected to large stains go away
+    clusters = label(img, connectivity=1)
+
+    props = regionprops(clusters)
+    diameters = np.array([prop.equivalent_diameter for prop in props])
+    euler_numbers = np.array([prop.euler_number for prop in props])
+    eccentricities = np.array([prop.eccentricity for prop in props])
+    areas = np.array([prop.filled_area for prop in props])
     cluster_sizes = np.bincount(clusters.ravel())
     size_threshold = threshold_otsu(cluster_sizes)
     print(size_threshold)
     is_big_enough = cluster_sizes > size_threshold
-    print(np.bincount(is_big_enough))
     out = np.copy(img)
     out[~is_big_enough[clusters]] = 255
     return out
 
+
+
+
     # Calculate mean width and height of clusters
+
+# We can characterise the blobs on the basis of eccentricty, area and diameter
+# We have the output images so we can learn the values from that data
+
+def local_otsu_denoising(img):
+    N = img.shape[0]
+    radius = np.arange(10, N/4, 10)
+    stds = []
+    out_images = []
+    for radii in radius:
+        selem = square(radii)
+
+        out = img.copy()
+        # Improve the contrast of the image
+
+        local_otsu = rank.otsu(img, selem)
+        stds.append(out[out <= local_otsu].std())
+        out[out >= local_otsu] = 255
+        out_images.append(out)
+    stds = np.array(stds)
+    arg = stds.argmin()
+    out = out_images[arg]
+
+    return out
+
+
+# After local_otsu_denoising you can find out the size of the letter then move
+# the a window of that size over the image and apply otsu there.
+# the size of the letter can median diameter of the bounding box
+# Or maybe we can do otsu again on large blobs
+
+# There are two cases the stain contains a letter or it does not contain a
+# letter
+
+def main(img):
+    denoised = local_otsu_denoising(img)
+
+    bin_denoised = np.copy(denoised)
+    bin_denoised[bin_denoised < 255] = 0
+
+    label_img = label(bin_denoised, connectivity=1)
+    props = np.array(regionprops(label_img))
+    diameters = np.array([prop.equivalent_diameter for prop in props])
+    euler_numbers = np.array([prop.euler_number for prop in props])
+    eccentricities = np.array([prop.eccentricity for prop in props])
+    areas = np.array([prop.filled_area for prop in props])
+
+    # XXX: 4 is a magic number
+    stain_labels = [prop.label for prop in props[areas > 4*np.median(areas)]]
+    stains = []
+    stain_threshold = []
+    for stain_label in stain_labels:
+        stains.append(label_img == stain_label)
+        stain_threshold.append(threshold_otsu(img[stains[-1]]))
+
+    # Remove stain from denoised image
+    # for stain, threshold in zip(stains, stain_threshold):
+    unstained = np.copy(denoised)
+    for stain, threshold in zip(stains, stain_threshold):
+        unstained[np.logical_and(stain, denoised > threshold)] = 255
+    plt.imshow(denoised, cmap='gray')
+    plt.show()
+    plt.imshow(unstained, cmap='gray')
+    plt.show()
 
 
 if __name__ == "__main__":
     images = get_data("./data/train/")
-    in_images = images[0]
 
-    out_images = []
-    radius = [10, 20, 30, 40, 50]
-    for radii in radius:
-        selem = square(radii)
+    img = images[9]
+    denoised = local_otsu_denoising(img)
 
-        local_otsu = rank.otsu(in_images, selem)
-        out = in_images.copy()
-        out[out <= local_otsu] = 0
-        out_images.append(out)
-    img = out_images[1]
-    out = remove_small_components(img)
+    bin_denoised = np.copy(denoised)
+    bin_denoised[bin_denoised < 255] = 0
+
+    bin_denoised = binary_erosion(bin_denoised)
+
+    label_img = label(bin_denoised, connectivity=1)
+    props = np.array(regionprops(label_img))
+    diameters = np.array([prop.equivalent_diameter for prop in props])
+    euler_numbers = np.array([prop.euler_number for prop in props])
+    eccentricities = np.array([prop.eccentricity for prop in props])
+    areas = np.array([prop.filled_area for prop in props])
+
+    # XXX: 4 is a magic number
+    stain_labels = [prop.label for prop in props[areas > 4*np.median(areas)]]
+    stains = []
+    stain_threshold = []
+    for stain_label in stain_labels:
+        stains.append(label_img == stain_label)
+        stain_threshold.append(threshold_otsu(img[stains[-1]]))
+
+    # Remove stain from denoised image
+    # for stain, threshold in zip(stains, stain_threshold):
+    unstained = np.copy(denoised)
+    for stain, threshold in zip(stains, stain_threshold):
+        unstained[np.logical_and(stain, denoised > threshold)] = 255
